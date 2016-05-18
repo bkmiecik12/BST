9062606e4f81aba90250c7a44495b1292e7b76f4
bkmiecik12 <bkmiecik12@gmail.com>
2016-05-18 14:18:25 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Bartłomiej on 11.05.2016.
+ */
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    public static  File file = new File("p.txt");
+    public static BST tree = new BST();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader s = new BufferedReader(new FileReader(file));
+        int i=1;
+        s.read();
+        while(s.ready()){
+
+            String line="";
+            line=s.readLine();
+            StringTokenizer st = new StringTokenizer(line," .,-");
+
+            while(st.hasMoreTokens()){
+                String word=st.nextToken();
+              //  System.out.println(word);
+                tree.insert(word,i);
+            }
+            //System.out.println("----"+i);
+            i++;
+        }
+        s.close();
+        //System.out.println(tree.root.word);
+
+        tree.display2(tree.root);
+        //tree.bfs(tree.root);
+        System.out.println();
+        tree.delete("nie");
+
+        tree.display2(tree.root);
+        System.out.println(tree.root.word);
+
+    }
+
+
+}
